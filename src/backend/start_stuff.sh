821d09c3c73ed737d9126f61c6108853e2f3d369
Joseph Li <lijoseph2003@gmail.com>
2024-05-05 05:01:58 -0500

    not

---
+++ src/backend/start_stuff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "hi"
+uvicorn app:app --reload
+gnome-terminal -- ./python random_player.py
+gnome-terminal -- ./python random_player.py
+gnome-terminal -- ./python random_player.py
+gnome-terminal -- ./python figgie_Newton.py
